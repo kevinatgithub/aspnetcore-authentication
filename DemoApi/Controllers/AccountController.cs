208f87c3972ae385f108baeeec0c8bf01c01424b
Kevin Cainday <Kevin.Cainday@ey.com>
2021-10-04 13:56:39 +0800

    wip

---
+++ DemoApi/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using DemoApi.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController: ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AccountController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Register(RegisterBindingModel model)
+        {
+            var user = new IdentityUser
+            {
+                Email = model.Email,
+                UserName = model.Email
+            };
+            var result = await userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                var code = await userManager.GenerateEmailConfirmationTokenAsync(user);
+                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+
+                return Ok(new { code = code, email = model.Email });
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
---
+++ DemoApi/Models/RegisterBindingModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoApi.Models
+{
+    public class RegisterBindingModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
